1adb6b82512f25acfc722c72d9b9997f72b22711
Ben Harris <harriben@iu.edu>
2019-09-18 13:16:40 -0400

    In-class work 09/16
    Event handlers and v-for in vue.js

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,14 +1,32 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatient: {
+      firstName: '',
+      lastName: '',
+      dob: '',
+      sexAtBirth: ''
+    }
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordApp.patients = json })
+    },
+    handleCreateRecord(event) {
+      this.patients.push(this.formPatient);
+      this.formPatient = {
+        firstName: '',
+        lastName: '',
+        dob: '',
+        sexAtBirth: ''
+      }
     }
+  },
+    handleRecordClick(patient) {
+      console.log(patient);
   },
   created() {
     this.fetchPatients();
